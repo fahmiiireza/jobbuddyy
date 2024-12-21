505fb94fc06e76fb93adf836e217cd3e860e1092
fahmiiireza <fahmi.fahreza@lastingdynamics.com>
2024-12-22 07:14:02 +0800

    disable admin and profile page

--- client/src/utils/links.jsx
+++ client/src/utils/links.jsx
@@ -10,8 +10,8 @@ const links = [
   { text: 'add job', path: '.', icon: <FaWpforms /> },
   { text: 'all jobs', path: 'all-jobs', icon: <MdQueryStats /> },
   { text: 'stats', path: 'stats', icon: <IoBarChartSharp /> },
-  { text: 'profile', path: 'profile', icon: <ImProfile /> },
-  { text: 'admin', path: 'admin', icon: <MdAdminPanelSettings /> },
+  // { text: 'profile', path: 'profile', icon: <ImProfile /> },
+  // { text: 'admin', path: 'admin', icon: <MdAdminPanelSettings /> },
 ];
 
 export default links;
